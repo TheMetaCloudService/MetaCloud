fc1e27895ec8ab13f59e9a1766f25d6117d90742
RauchigesEtwas <bohlandrobin.rb@gmail.com>
2024-03-16 12:28:36 +0100

    fixes false packet handling

--- metacloud-api/src/main/java/eu/metacloudservice/networking/HandlePacketOutGroupEdit.java
+++ metacloud-api/src/main/java/eu/metacloudservice/networking/HandlePacketOutGroupEdit.java
@@ -3,12 +3,15 @@ package eu.metacloudservice.networking;
 import eu.metacloudservice.CloudAPI;
 import eu.metacloudservice.events.listeners.group.CloudGroupUpdateEditEvent;
 import eu.metacloudservice.networking.packet.packets.out.service.group.PacketOutGroupDelete;
+import eu.metacloudservice.networking.packet.packets.out.service.group.PacketOutGroupEdit;
 import io.netty.channel.Channel;
 import eu.metacloudservice.networking.packet.NettyAdaptor;
 import eu.metacloudservice.networking.packet.Packet;
 public class HandlePacketOutGroupEdit implements NettyAdaptor {
     @Override
     public void handle(Channel channel, Packet packet) {
-        CloudAPI.getInstance().getEventDriver().executeEvent(new CloudGroupUpdateEditEvent(((PacketOutGroupDelete) packet).getGroup()));
+        if (packet instanceof PacketOutGroupEdit){
+            CloudAPI.getInstance().getEventDriver().executeEvent(new CloudGroupUpdateEditEvent(((PacketOutGroupEdit) packet).getGroup()));
+        }
     }
 }
